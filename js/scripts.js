e800ee731d0c9cde503f487697300f5a4ffde1a4
mthomp35 <mthomp35@users.noreply.github.com>
2020-11-19 20:41:54 -0800

    Create variable pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+  { name: 'Charizard', height: 20, types: ['fire', 'flying'] },
+  { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
+]
