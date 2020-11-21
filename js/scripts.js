302858a5eb5d13b23eb1a6200207492d9e9229db
mthomp35 <mthomp35@users.noreply.github.com>
2020-11-21 14:55:20 -0800

    Added loop to display list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ let pokemonList = [
   { name: 'Charizard', height: 20, types: ['fire', 'flying'] },
   { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
 ]
+
+for (let i=0; i < pokemonList.length; i++){
+  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>')
+}
