4cc8c189d697004f2bd7da01400ec31a27afd6ad
mthomp35 <mthomp35@users.noreply.github.com>
2020-11-21 21:41:59 -0800

    update js code format

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonList = [
   { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
   { name: 'Charizard', height: 20, types: ['fire', 'flying'] },
   { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
-]
+];
 
 // write list of pokemon with height, calling out those over 15 as big
 for (let i=0; i < pokemonList.length; i++){
