dc406f9c5852d0e30847671532218f451276f1b1
mthomp35 <mthomp35@users.noreply.github.com>
2020-11-27 19:02:53 -0800

    Add a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,17 @@ let pokemonList = [
   { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
 ];
 
+//write list of pokemon using forEach
+pokemonList.forEach(function (pokemon) {
+    document.write(pokemon.name + ', height: ' + pokemon.height + ', ' + pokemon.types[0] + ', ' + pokemon.types[1] + '<br>');
+});
+
+
 // write list of pokemon with height, calling out those over 15 as big
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height >=15){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s a Big Pokémon!</p>')
-  }else {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>')
-  }
-}
+// for (let i=0; i < pokemonList.length; i++){
+  // if (pokemonList[i].height >=15){
+    // document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s a Big Pokémon!</p>')
+  // }else {
+    // document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>')
+  // }
+// }
