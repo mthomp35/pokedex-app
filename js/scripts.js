a53cda9f3c8ec94d71a9c42002039f4eb2b69b8e
mthomp35 <mthomp35@users.noreply.github.com>
2020-11-28 20:10:27 -0800

    create iife pokemon repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
-// create array of pokemon
-let pokemonList = [
-  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-  { name: 'Charizard', height: 20, types: ['fire', 'flying'] },
-  { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
-];
+// wrap pokemon repository in iife
+let pokemonRepository = (function () {
+  // create array of pokemon
+  let pokemonList = [
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { name: 'Charizard', height: 20, types: ['fire', 'flying'] },
+    { name: 'Slowpoke', height: 11, types: ['water', 'psychic'] }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function remove(pokemon) {
+    pokemonList.pull(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    remove: remove,
+    getAll: getAll
+  };
+
+})();
+
+
 
 //write list of pokemon using forEach
 pokemonList.forEach(function (pokemon) {
