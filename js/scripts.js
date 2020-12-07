1b0ec476e7343bb2524322b2ba6bb1cf3089d7b8
mthomp35 <mthomp35@users.noreply.github.com>
2020-12-06 20:25:58 -0800

    Revert "attempt to create separate function for button event listener"
    This reverts commit 0810a5eabba09106a6f5dc8a5e347a9fc12d90e1.

--- js/scripts.js
+++ js/scripts.js
@@ -40,20 +40,15 @@ let pokemonRepository = (function () {
       button.classList.add('pokemon-button');
       listItem.appendChild(button);
       listElement.appendChild(listItem);
-      pokemonButton(pokemon);
-    //  button.addEventListener('click', function (event) {
+      button.addEventListener('click', function (event) {
         // show details of the pokemon selected when button is clicked
-    //    showDetails(pokemon);
-    //  });
+        showDetails(pokemon);
+      });
   }
 
   // show details of the pokemon
   function showDetails(pokemon) {
     console.log(pokemon);
-  }
-
-  function pokemonButton(pokemon) {
-    addListItem.button.addEventListener('click', showDetails);
   }
 
   return {
